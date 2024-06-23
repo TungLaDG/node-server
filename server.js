28de79acf9a1264667d8df961ce673f07b093f4a
Tung La <latung2806@gmail.com>
2024-06-23 16:13:18 +0700

    remove private key

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const admin = require('firebase-admin');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Node Server');
+const serviceAccount = require('./net-store-angular-firebase-adminsdk-gpzs0-64b7543b2b.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+app.get('/api/users', async (req, res) => {
+  try {
+    const usersSnapshot = await db.collection('users').get();
+    const users = usersSnapshot.docs.map(doc => doc.data());
+    res.json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).send('Error fetching users');
+  }
 });
 
 app.listen(port, () => {
